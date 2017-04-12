d8d4733b46d78e119a1b09d13b21ed0a41b13e49
Sean Rivis <sean.rivis@gmail.com>
2017-04-12 16:11:19 -0400

    change colors and wording to Our MS

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 //variables
-
+var introSpot = document.getElementsByClassName('intro')[0];
 //functions
-
+// window.load(function() {
+//     introSpot.removeClass('hold');
+// })
 //script
+
+
+//gsap
+TweenMax.to(".columnOne", 5, {color:"rgba(255,0,255,30)"}, {delay:2});
+TweenMax.to('.columnTwo',1, {color:"rgba(144,144,20,10)"}, {delay:2});
+
